c422545163584840f3cd5dbfaf6ac851456217aa
uiwns811 <dltnals5809@gmail.com>
2020-09-18 19:41:14 +0900

    last assignment

---
+++ Assignment/200907/py_02_03_2019182026_1.py
@@ -0,0 +1,107 @@
+<<<<<<< HEAD
+import turtle
+
+turtle.penup()
+turtle.goto(-350, 0)
+turtle.pendown()
+turtle.circle(100)
+turtle.penup()
+turtle.goto(-200,200)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(200)
+ 
+turtle.penup()
+turtle.goto(0,200)
+turtle.goto(-50,200)
+turtle.right(45)
+turtle.pendown()
+turtle.forward(100)
+turtle.backward(70)
+turtle.left(90)
+turtle.forward(70)
+turtle.penup()
+turtle.goto(-100,100)
+turtle.left(45)
+turtle.backward(50)
+turtle.pendown()
+turtle.forward(175)
+turtle.backward(85)
+
+turtle.right(90)
+turtle.forward(85)
+turtle.penup()
+turtle.goto(100,200)
+ 
+turtle.pendown()
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+	
+turtle.penup()
+turtle.goto(250,200)
+turtle.pendown()
+turtle.forward(150)
+turtle.penup()
+turtle.goto(100,75)
+turtle.pendown()
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(150)
+
+turtle.exitonclick()
+=======
+import turtle
+
+turtle.penup()
+turtle.goto(-350, 0)
+turtle.pendown()
+turtle.circle(100)
+turtle.penup()
+turtle.goto(-200,200)
+turtle.right(90)
+turtle.pendown()
+turtle.forward(200)
+ 
+turtle.penup()
+turtle.goto(0,200)
+turtle.goto(-50,200)
+turtle.right(45)
+turtle.pendown()
+turtle.forward(100)
+turtle.backward(70)
+turtle.left(90)
+turtle.forward(70)
+turtle.penup()
+turtle.goto(-100,100)
+turtle.left(45)
+turtle.backward(50)
+turtle.pendown()
+turtle.forward(175)
+turtle.backward(85)
+
+turtle.right(90)
+turtle.forward(85)
+turtle.penup()
+turtle.goto(100,200)
+ 
+turtle.pendown()
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+
+	
+turtle.penup()
+turtle.goto(250,200)
+turtle.pendown()
+turtle.forward(150)
+turtle.penup()
+turtle.goto(100,75)
+turtle.pendown()
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(150)
+
+turtle.exitonclick()
+>>>>>>> caa9190ac275e1eeb9e0127034cd6b1ea00d411a
---
+++ Assignment/200907/py_02_03_2019182026_2.py
@@ -0,0 +1,59 @@
+<<<<<<< HEAD
+import turtle
+
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+
+for i in range(5):
+    turtle.forward(100)
+    for i in range(2):
+        turtle.left(90)
+        turtle.forward(500)
+        turtle.left(90)
+        turtle.forward(100)
+
+
+turtle.left(90)
+
+for i in range(4):
+    turtle.forward(100)
+    for i in range(2):
+        turtle.left(90)
+        turtle.forward(500)
+        turtle.left(90)
+        turtle.forward(100)
+
+		
+turtle.exitonclick()
+
+=======
+import turtle
+
+turtle.penup()
+turtle.goto(-200, -200)
+turtle.pendown()
+
+for i in range(5):
+    turtle.forward(100)
+    for i in range(2):
+        turtle.left(90)
+        turtle.forward(500)
+        turtle.left(90)
+        turtle.forward(100)
+
+
+turtle.left(90)
+
+for i in range(4):
+    turtle.forward(100)
+    for i in range(2):
+        turtle.left(90)
+        turtle.forward(500)
+        turtle.left(90)
+        turtle.forward(100)
+
+		
+turtle.exitonclick()
+
+>>>>>>> caa9190ac275e1eeb9e0127034cd6b1ea00d411a
