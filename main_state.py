4e256871877a4f89559e4d5c4704d6f23b1474ca
uiwns811 <71070963+uiwns811@users.noreply.github.com>
2020-11-27 06:28:04 +0900

    STAIR_WIDTH/HEIGHT 를 image.w / image.h로 변경

---
+++ main_state.py
@@ -0,0 +1,136 @@
+import gfw
+from pico2d import *
+import background
+from player import Player
+from stair import *
+import stair_gen
+from coins import *
+import gobj
+import time
+import random
+import collision
+
+canvas_width = 700
+canvas_height = 1000
+
+def build_world():
+    global player
+    gfw.world.init(['bg', 'stairs', 'coins', 'player'])
+
+    generate_player()
+    generate_stair()
+    
+    # global bg
+    # bg = FixedBackground('/background.png')
+    # gfw.world.add(gfw.layer.bg, bg)
+    background.init()
+    gfw.world.add(gfw.layer.bg, background)
+
+    global font
+    font = gfw.font.load('../res/font' + '/ARCADECLASSIC.TTF', 40)
+
+def generate_player():
+    global player, bg
+    player = Player()
+    player.bg = background
+    gfw.world.add(gfw.layer.player, player)
+    bb = player.get_bb()
+    print('player =', bb)
+  
+def generate_stair():
+    print('*****************************hi*************************')    
+
+    # stair_gen.init()    global stairs
+    # for s in range (0, 13):
+    stairs = stair_gen.update()
+    print("generate_stair")
+
+    # global coins
+    # sc = random.randint(1, 5)
+    # if sc == 3:
+    #     coins = Coins((stairs.x, stairs.y), (15, 15))
+    #     gfw.world.add(gfw.layer.coins, coins)
+    # for s in gfw.world.objects_at(gfw.layer.stairs):
+    #     bb = Stair.get_bb(s)
+    #     print(bb)
+
+    
+    # global stairs, bg
+    # for e in range(0, 15):
+    #     stairs = Stair()
+    # # stairs.bg = bg
+    #     gfw.world.add(gfw.layer.stairs, stairs)
+
+    # for stairs in gfw.world.objects_at(gfw.layer.stairs):
+    #     gfw.world.add(gfw.layer.stairs, stairs)
+
+def enter():
+    center = get_canvas_width() // 2, get_canvas_height() // 2
+
+    build_world()
+    # generate_player()
+    generate_stair()
+    # for e in range (0, 20):
+
+def update():
+    #generate_stair()
+    gfw.world.update()
+    # time.sleep(1)
+
+    remove()
+    # stairs_gen.update()
+
+def draw():
+    gfw.world.draw()
+    gobj.draw_collision_box()
+    font.draw(20, canvas_height - 45, "hello")
+
+def handle_event(e):
+    c_level = 4
+    # prev_dx = boy.dx
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE: 
+            gfw.pop()
+        elif e.key == SDLK_DOWN:
+            generate_stair()
+    if e.type == SDL_MOUSEBUTTONDOWN:
+        for s in gfw.world.objects_at(gfw.layer.stairs):
+            if s.ylevel == c_level:
+                # Stair.move_pos(s)
+                check_stairs(s)
+                if check_stairs(s) == True:
+                    for sa in gfw.world.objects_at(gfw.layer.stairs):
+                        Stair.move_pos(sa)
+                    c_level += 1
+
+            # bb = Stair.get_bb(s)
+            # print(bb)
+            # sc = gfw.world.count_at(gfw.layer.stairs)
+            # print('sc : ', sc)
+
+    # if stairs.handle_event(e):
+    #     return
+
+def check_stairs(e):
+    if gobj.collides_box(player, e):
+        print('Stairs Collision', e)
+        return True;
+    else:
+        return False;
+
+def remove():
+    global stairs
+    if gfw.world.count_at(gfw.layer.stairs) > 12:
+        gfw.world.remove(gfw.layer.stairs)
+        gfw.world.empty_trashcan()
+        # print('================================================')
+        # print(gfw.world.count_at(gfw.layer.stairs))
+        # print('================================================')
+
+def exit():
+    pass
+
+if __name__ == '__main__':
+    gfw.run_main()
---
+++ stair_gen.py
@@ -0,0 +1,59 @@
+from pico2d import *
+import gfw
+from stair import Stair
+import random
+
+STAIR_COUNT = 13
+
+SPEED_PPS = 3000
+MAG_SPEED = 0.15
+stair_level = 0
+# STAIR_WIDTH = 118 132
+# STAIR_HEIGHT = 65 72
+
+prev_index = random.randint(0, 4)  # 이전 계단의 인덱스
+cur_index = 0   # 현재 계단의 인덱스
+cnt_dec = 0     # 왼쪽으로의  증가폭
+cnt_inc = 0     # 오른쪽으로의 증가폭 
+
+# 매 프레임마다 불리는 함수, 최초에 한 번만 불리는 함수
+def init():
+    pass
+
+def update():
+    while gfw.world.count_at(gfw.layer.stairs) < STAIR_COUNT:
+        generate()
+
+def generate():
+    x, y, stair_ylevel, xdirection = get_coords()
+    print(xdirection)
+    s = Stair((x,y), stair_ylevel, xdirection)     # pos, delta가 인자이므로 튜플로 묶어서 전달
+    gfw.world.add(gfw.layer.stairs, s)
+
+def get_coords():           # 좌표를 생성하는 함수를 만들자.. missile말고 item도 만들건데 생성되는 x, y, dx, dy를 결정하는게 따로 필요.
+    global stair_level, prev_index, cur_index
+    image = gfw.image.load('../res/image/stairs.png')
+    xdirection = random.randint(0, 1)      # 계단의 생성 방향 결정
+    if xdirection == 0:
+        if prev_index < 1:      # 더이상 왼쪽으로 갈 수 없는 경우
+            cur_index = prev_index + 1
+            prev_index += 1
+            xdirection = 1
+        else:
+            cur_index = prev_index - 1
+            prev_index -= 1
+
+    if xdirection == 1:
+        if prev_index > 3:      # 더이상 오른쪽으로 갈 수 없는 경우
+            cur_index = prev_index - 1
+            prev_index -= 1
+            xdirection = 0
+        else:
+            cur_index = prev_index + 1
+            prev_index += 1
+
+    x = (cur_index) * image.w     # x의 화면상의 위치 표시
+    y = stair_level * image.h + 120  # y의 화면상의 위치 표시
+    stair_level += 1        #계단의 높이 (최대 12)
+    
+    return x, y, stair_level, xdirection
